de0cfd654f12bee1f7dd7cc351860023c7a7833b
Manish <manishekaneja@gmail.com>
2018-03-04 21:29:46 +0530

    IMP

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/'));
 app.get("/", function (req, res) {
