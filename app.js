8693d6843c33f54289fe1ad4f4268c42cda5cd8d
Manish <manishekaneja@gmail.com>
2018-03-04 21:47:59 +0530

    httpsTry2

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 app.set('port', (process.env.PORT || 5000));
-app.enable('trust proxy');
+    app.use(function (req, res, next) {
+        var schema = (req.headers['x-forwarded-proto'] || '').toLowerCase();
+        if (schema === 'https') {
+            next();
+        } else {
+            res.redirect('https://' + req.headers.host + req.url);
+        }
+    });
 app.use(express.static(__dirname + '/'));
 app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname + "/index.html"));
