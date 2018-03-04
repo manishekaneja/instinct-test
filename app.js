f110d0fb01a1f6f96f60fb03833434a9feb630ab
Manish <manishekaneja@gmail.com>
2018-03-04 21:19:37 +0530

    nodefile

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/'));
+app.get("/", function (req, res) {
+    res.sendFile(path.resolve(__dirname + "/index.html"));
+})
+app.listen(app.get('port'), function () {
+    console.log("Server Running at : " + app.get('port'));
+})
