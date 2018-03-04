c6f4ca21d429e372bcf12d784f5045069816d271
Manish <manishekaneja@gmail.com>
2018-03-04 21:41:11 +0530

    httpsTry

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.set('port', (process.env.PORT || 5000));
+app.enable('trust proxy');
 app.use(express.static(__dirname + '/'));
 app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname + "/index.html"));
